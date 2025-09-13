0ee4b22a468483a07adcf07ce4dc7d0ea95d58ed
Saidkamol <kamolsaid2006@gmail.com>
2025-09-13 14:34:08 +0500

    feat: add models dir and base DB model

---
+++ src/db/models/base.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy import MetaData
+
+from core.config import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f""
