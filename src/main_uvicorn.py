d523df03eb5407ab917c214c04b2b1180572830f
Saidkamol <kamolsaid2006@gmail.com>
2025-09-13 14:34:51 +0500

    feat: add creating app and running with uvicorn

---
+++ src/main_uvicorn.py
@@ -0,0 +1,35 @@
+import logging
+import uvicorn
+
+from core.config import settings
+from app import create_app
+
+
+logging.basicConfig(
+    filename=settings.logging.log_file if not settings.run.debug else None,
+    level=settings.logging.log_level_value,
+    datefmt=settings.logging.date_format,
+    format=settings.logging.log_format,
+)
+
+main_app = create_app(
+    create_custom_static_urls=True,
+)
+
+
+@main_app.get("/")
+async def root():
+    return {"msg": "Hello, world!"}
+
+
+def uvicorn_run():
+    uvicorn.run(
+        "main_uvicorn:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=settings.run.reload,
+    )
+
+
+if __name__ == "__main__":
+    uvicorn_run()
