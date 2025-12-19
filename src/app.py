9d18ec320c3685ae050b14b21f0d5a44c3878bf9
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2025-12-19 12:21:18 +0500

    refactor: update app creating and setup

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
-import logging
+from __future__ import annotations
+
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator, Dict
+from typing import AsyncGenerator
 
 from fastapi import FastAPI
 from fastapi.openapi.docs import (
@@ -8,14 +9,12 @@ from fastapi.openapi.docs import (
     get_swagger_ui_html,
     get_swagger_ui_oauth2_redirect_html,
 )
-from fastapi.responses import ORJSONResponse
+from fastapi.responses import ORJSONResponse, RedirectResponse
 from starlette.responses import HTMLResponse
 
 from api import router as main_router
 from core.exception import add_exception_handlers
 from db import db_helper
-
-log = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -38,16 +37,8 @@ def register_static_docs_routes(app: FastAPI) -> None:
             swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
         )
 
-    app.add_api_route("/docs", custom_swagger_ui_html, include_in_schema=False)
-
     async def swagger_ui_redirect() -> HTMLResponse:
         return get_swagger_ui_oauth2_redirect_html()
-
-    app.add_api_route(
-        str(app.swagger_ui_oauth2_redirect_url),
-        swagger_ui_redirect,
-        include_in_schema=False,
-    )
 
     @app.get("/redoc", include_in_schema=False)
     async def redoc_html() -> HTMLResponse:
@@ -57,20 +48,16 @@ def register_static_docs_routes(app: FastAPI) -> None:
             redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
         )
 
+    app.add_api_route("/docs", custom_swagger_ui_html, include_in_schema=False)
+    app.add_api_route(
+        str(app.swagger_ui_oauth2_redirect_url),
+        swagger_ui_redirect,
+        include_in_schema=False,
+    )
     app.add_api_route("/redoc", redoc_html, include_in_schema=False)
 
 
-def config_app(app: FastAPI) -> None:
-    add_exception_handlers(app=app)
-
-    async def root() -> Dict[str, str]:
-        return {"message": "Hello, World!"}
-
-    app.add_api_route("/", root)
-    app.include_router(main_router)
-
-
-def create_app(
+def create_and_setup_app(
     create_custom_static_urls: bool = False,
 ) -> FastAPI:
     app = FastAPI(
@@ -82,6 +69,17 @@ def create_app(
     if create_custom_static_urls:
         register_static_docs_routes(app)
 
-    config_app(app)
+    add_exception_handlers(app=app)
 
+    async def root():
+        return RedirectResponse(url="/docs")
+
+    app.add_api_route(
+        "/",
+        root,
+        response_class=RedirectResponse,
+        status_code=307,
+    )
+
+    app.include_router(main_router)
     return app
