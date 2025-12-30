414e458cd3d47d9529112c774737abd0304a787c
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2025-12-30 13:51:29 +0500

    refactor: improve exception handler setting

--- src/core/exception/handler.py
+++ src/core/exception/handler.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from starlette import status
 
 from .exceptions import (
@@ -13,13 +13,13 @@ from .exceptions import (
 
 def add_exception_handlers(app: FastAPI):
     def register(exc_class: type[Exception], status_code):
-        async def handler(request: Request, exc: type[Exception]):
-            return JSONResponse(
-                {"detail": str(exc)},
+        app.add_exception_handler(
+            exc_class,
+            lambda request, exc: ORJSONResponse(
+                content={"detail": str(exc)},
                 status_code=status_code,
-            )
-
-        app.add_exception_handler(exc_class, handler)
+            ),
+        )
 
     register(BadRequestError, status.HTTP_400_BAD_REQUEST)
     register(ForbiddenError, status.HTTP_403_FORBIDDEN)
